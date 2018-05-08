2a6c26ebc04b264fa165346345519db72355fd7e
Ludmila <luvi_lv@inbox.lv>
2018-05-08 21:38:16 +0300

    3.5
    var 1 man ir suns

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -23,6 +23,14 @@ namespace ConsoleApp1
             Console.ReadLine();
             string result = Funkcija5("Ludzu ","iedodiet ","gramatu ");
             Console.WriteLine(result);
+            
+
+            string var1, var2, var3;
+            var1 = Console.ReadLine();
+            var2 = Console.ReadLine();
+            var3 = Console.ReadLine();
+            result = Funkcija5(var1, var2, var3);
+            Console.WriteLine(result);
             Console.ReadLine();
 
         }
