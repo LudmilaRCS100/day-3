ffd537845bbac41d93d86e1ea06834edebe25408
Ludmila <luvi_lv@inbox.lv>
2018-05-08 20:36:02 +0300

    3.3
    funkcijas 3 un 4 result

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace ConsoleApp1
 {
     class Program
+
     {
         static void Main(string[] args)
         {
@@ -15,7 +16,12 @@ namespace ConsoleApp1
 
 
             Funkcija();
+            Funkcija3();
+            int mainigais2=Funkcija4(12,14);
+
+            Console.WriteLine(mainigais2);
             Console.ReadLine();
+
         }
         static void Funkcija()
         {
@@ -38,13 +44,31 @@ namespace ConsoleApp1
             c = a + b;
             return c;
         }
+        static void Funkcija3()
+
+        {
+            int skaitlis1, skaitlis2, d;
+            skaitlis1 = 2;
+            skaitlis2 = 3;
+            d= skaitlis1 + skaitlis2;
+            Console.WriteLine(d);
+
+            return;
+
+
+        }
+        static int Funkcija4(int skaitlis1, int skaitlis2)
+        {
+            int result;
+            result = skaitlis1 + skaitlis2;
+            return result;
+        }
 
 
 
 
 
         
-            int a, b, c;
-
+           
     }
 }
